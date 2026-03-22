7afcd59ac503c9ee57feba7ec736e3ae094e5ef0
Johnny <johnnylin310@gmail.com>
2026-03-22 14:56:16 -0700

    Restructured and finalized Preprocessing module for baseline tests

--- src/Modeling.py
+++ src/Modeling.py
@@ -1,9 +1,7 @@
 import pandas as pd
+from src.Preprocessing.main import *
 
 def load_data():
     df = pd.read_parquet('../Data/clean_data.parquet')
     return df
 
-######### Johnny Part
-
-df = load_data()
